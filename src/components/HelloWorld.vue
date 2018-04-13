a7dfe397ccbfb8d3a3e9cac963e36d82e40626d7
sylenien <sylenien@gmail.com>
2018-04-14 02:08:19 +0300

    vue files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <div style="width: 640px; height: 480px" id="mapContainer"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted() {
+    var platform = new H.service.Platform({
+      'app_id': 'j4o3XiGsSiUtrrUyy1jp',
+      'app_code': 'rXPXKkeiuAB4YtVt1zdeqg'
+      });
+    
+      // Obtain the default map types from the platform object
+      var maptypes = platform.createDefaultLayers();
+
+      // Instantiate (and display) a map object:
+      var map = new H.Map(
+      document.getElementById('mapContainer'),
+      maptypes.normal.map,
+      {
+      zoom: 10,
+      center: { lng: 13.4, lat: 52.51 }
+      });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
